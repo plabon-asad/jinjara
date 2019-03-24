2dcc0a57f3c19d2fd6fae8581f3304384c3c0cba
plabon-asad <plabondotasad@gmail.com>
2019-03-24 15:31:27 +0600

    theme folder structure ready

--- wp-content/themes/jinjara/page.php
+++ wp-content/themes/jinjara/page.php
@@ -1,21 +1,4 @@
-<?php
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage Twenty_Seventeen
- * @since 1.0
- * @version 1.0
- */
-
-get_header(); ?>
+<?php get_header(); ?>
 
 <div class="wrap">
 	<div id="primary" class="content-area">
@@ -38,4 +21,4 @@ get_header(); ?>
 	</div><!-- #primary -->
 </div><!-- .wrap -->
 
-<?php get_footer();
+<?php get_footer(); ?>
